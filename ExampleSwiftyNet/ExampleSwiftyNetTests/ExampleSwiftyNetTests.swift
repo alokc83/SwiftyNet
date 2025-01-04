54115671a97b1922e36b753e885315877b44f75c
Alok Choudhary <alok.c83@gmail.com>
2025-01-04 14:37:29 -0600

    Moved FW to folder. Created example project.

---
+++ ExampleSwiftyNet/ExampleSwiftyNetTests/ExampleSwiftyNetTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleSwiftyNetTests.swift
+//  ExampleSwiftyNetTests
+//
+//  Created by Alok Choudhary on 1/4/25.
+//
+
+import Testing
+
+struct ExampleSwiftyNetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
